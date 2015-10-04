6749c0d20a1817ce2377d97bc8bb84c66d96fba0
Abhinav Madahar <abhinavmadahar@gmail.com>
2015-10-04 06:06:48 -0400

    Implement simple search by tag API

--- server.js
+++ server.js
@@ -1,14 +1,39 @@
 // use babel-node to run this file
 
 import express from "express";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+import logarithmic from "logarithmic";
+import Image from "./models/image";
+
+mongoose.connect("localhost");
+
+mongoose.connection.on("error", () => {
+  logarithmic.error("Could not connect to Mongoose");
+});
 
 const app = express();
 
 app.use(express.static(`${__dirname}/public`));
+app.use(bodyParser());
 
-app.get("/api/related/:sentence", (request, response) => {
-  response.send("Hello");
-  console.log(request.params.imageURL);
+mongoose.connection.on("open", () => {
+  logarithmic.ok("Connected to Mongoose correctly");
+  app.get("/api/related/", (request, response) => {
+    const keywords = request.query.keywords.split(",").map((word) => word.trim());
+    Image.find({}, (error, images) => {
+      const isNotInArray = (array) => {
+        return (element) => array.indexOf(element) === -1;
+      };
+      let relatedImages = [];
+      for (let image of images) {
+        if (keywords.filter(isNotInArray(image.keywords)).length === 0) { // if all keywords are in the image
+          relatedImages.push(image);
+        }
+      }
+      response.send(relatedImages);
+    });
+  });
 });
 
 app.get("/", (request, response) => {
@@ -16,5 +41,5 @@ app.get("/", (request, response) => {
 });
 
 app.listen(8080, () => {
-  console.log("Server has started up");
+  logarithmic.ok("Server has started up");
 });
